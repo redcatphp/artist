03de76ba9b29f9243d42a6fcc07cf57a03575690
surikat <jo@surikat.pro>
2016-06-26 01:56:24 +0200

    fix

--- install-project.php
+++ install-project.php
@@ -3,11 +3,11 @@ $lines = file('bin/create-project');
 $tmp = [];
 $tmp[] = array_shift($lines);
 $tmp[] = array_shift($lines);
-$i = 1;
+$i = count($_REQUEST);
 foreach(array_reverse($_REQUEST) as $k=>$v){
 	$val = $v?$k.'='.$v:$k;
 	array_unshift($lines,'$argv['.$i.'] = "'.str_replace('"','\"',$val).'";');
-	$i++;
+	$i--;
 }
 array_unshift($lines,array_pop($tmp));
 array_unshift($lines,array_pop($tmp));
