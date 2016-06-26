f836a9a69072214fb378b55b4ed911c15f7f906d
surikat <jo@surikat.pro>
2016-06-26 14:17:21 +0200

    wip

--- src/ComposerArtist.php
+++ src/ComposerArtist.php
@@ -2,7 +2,7 @@
 namespace RedCat\Artist;
 class ComposerArtist{
 	static function __callStatic($func,$args){
-		$php = 'bin/artist';
+		$php = 'packages/bin/artist';
 		$_SERVER['argv'] = $GLOBALS['argv'] = [$php,$func];
 		ob_start();
 		include $php;
