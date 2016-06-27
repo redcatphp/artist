5a6b00c58ac45f6f613d03f845aa6035d183947b
surikat <jo@surikat.pro>
2016-06-27 10:08:08 +0200

    improve + fixs

--- src/ComposerArtist.php
+++ src/ComposerArtist.php
@@ -5,8 +5,12 @@ class ComposerArtist{
 		$event = array_shift($args);
 		$GLOBALS['ioDialogRedCat'] = $event->getIO();
 		$php = 'packages/bin/artist';
+		$func = self::snakeCase($func);
 		$_SERVER['argv'] = $GLOBALS['argv'] = [$php,$func];
 		ob_start();
 		include $php;
+	}
+	static function snakeCase($str){
+		return str_replace(' ', ':', strtolower(preg_replace('/([a-z])([A-Z])/', '$1 $2', $str)));
 	}
 }
